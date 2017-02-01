87e6ccdad1641576f71635c36398acae43f58f56
James Till Matta <jamesmatta@gmail.com>
2017-02-01 18:19:35 -0500

    Fixed a comment
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Events/OrchidSlowControlsEvent.h
+++ src/Events/OrchidSlowControlsEvent.h
@@ -12,7 +12,7 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 **
-** @details Holds the class for DPP-PSD events without waveforms
+** @details Holds the class for ORCHID slow controls events
 **
 ********************************************************************************
 *******************************************************************************/
