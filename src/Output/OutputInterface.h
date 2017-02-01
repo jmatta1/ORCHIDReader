676d150bd8a85be3497f4c7b9136a70be84e7df5
James Till Matta <jamesmatta@gmail.com>
2017-02-01 10:03:58 -0500

    Interface for output classes
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Output/OutputInterface.h
@@ -0,0 +1,43 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file OutputInterface.h
+** @author James Till Matta
+** @date 31 Jan, 2017
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the abstract base class that all output systems must follow
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHIDREADER_SRC_OUTPUT_OUTPUTINTERFACE_H
+#define ORCHIDREADER_SRC_OUTPUT_OUTPUTINTERFACE_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHIDReader
+#include"Events/OrchidSlowControlsEvent.h"
+#include"Events/DppPsdIntegralEvent.h"
+
+namespace Output
+{
+
+class OutputInterface
+{
+public:
+    virtual ~OutputInterface(){}
+    
+    virtual void slowControlsEvent(const Events::OrchidSlowControlsEvent& event) = 0;
+    virtual void dppPsdIntegralEvent(const Events::DppPsdIntegralEvent& event) = 0;
+    virtual void done() = 0;
+};
+
+}
+
+#endif //ORCHIDREADER_SRC_OUTPUT_OUTPUTINTERFACE_H
