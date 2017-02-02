5fa20a1b05652984b43206e6c279a8b51753ee09
James Till Matta <jamesmatta@gmail.com>
2017-02-02 09:23:37 -0500

    Defined the InputFileSwapEvent, no need for impl
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ src/Events/InputFileSwapEvent.h
@@ -0,0 +1,39 @@
+/***************************************************************************//**
+********************************************************************************
+**
+** @file InputFileSwapEvent.h
+** @author James Till Matta
+** @date 02 Feb, 2017
+** @brief
+**
+** @copyright Copyright (C) 2016 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+**
+** @details Holds the definition of the class for file swaps
+**
+********************************************************************************
+*******************************************************************************/
+#ifndef ORCHIDREADER_SRC_EVENTS_INPUTFILESWAPEVENT_H
+#define ORCHIDREADER_SRC_EVENTS_INPUTFILESWAPEVENT_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from ORCHIDReader
+
+namespace Events
+{
+
+class InputFileSwapEvent
+{
+public:
+    unsigned long long oldFileLastBufferTime;
+    unsigned long long newFileHeaderTime;
+    bool sameRun;
+};
+
+}
+
+#endif //ORCHIDREADER_SRC_EVENTS_DPPPSDINTEGRALEVENT_H
